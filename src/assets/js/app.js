b3be206a11bb9c7d088dfab171c1a8535415bf19
Greg <greg@computer-shoppe.net>
2017-08-09 21:15:46 -0700

    added many changes to the primary menu

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,3 +10,46 @@ import Foundation from 'foundation-sites';
 
 
 $(document).foundation();
+
+
+
+
+//testing
+$(function() {
+  $(window).scroll(function() {
+  	//alert("function called");
+    var winTop = $(window).scrollTop();  // for chrome
+    if (winTop >= 30 || document.documentElement.scrollTop >= 30) {
+      //alert("class added");	
+      //$("body").addClass("sticky-shrinknav-wrapper");
+      document.getElementById('animated-menu').classList.remove('nav__nav-bar--primary-loaded');
+      document.getElementById('animated-menu').classList.add('nav__nav-bar--primary-moved');
+    } else {
+      //alert("class removed");	
+      // $("body").removeClass("sticky-shrinknav-wrapper");
+      document.getElementById('animated-menu').classList.add('nav__nav-bar--primary-loaded');
+      document.getElementById('animated-menu').classList.remove('nav__nav-bar--primary-moved');
+
+    }
+  });
+});
+
+//                     document.getElementById('animated-menu').classList.add('nav__nav-bar--primary');
+
+// $(function() {
+//   $(window).scroll(function() {
+//     var winTop = $(window).scrollTop();
+//     if (winTop >= 30) {
+//       $("body").addClass("sticky-shrinknav-wrapper");
+//     } else{
+//       $("body").removeClass("sticky-shrinknav-wrapper");
+//     }
+//   });
+// });
+
+
+
+
+
+
+
